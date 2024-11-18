4397cd8fd51cc8e065354587cd32895d69a8c961
dlsdndlsdn <hwang-in-woo@hwang-in-uui-MacBookAir.local>
2024-11-18 16:40:07 +0900

    commit check

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -1,7 +1,8 @@
 package com.ll;
-
+// 명언 앱
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Hello, World!");
         System.out.println("Hello, World!");
     }
 }
