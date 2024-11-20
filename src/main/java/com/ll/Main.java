062bced4fd9dfcfdfde4aa1168b2092a0ac7e785
dlsdndlsdn <hwang-in-woo@hwang-in-uui-MacBookAir.local>
2024-11-20 18:10:13 +0900

    9단계 text파일로 완성

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -1,6 +1,8 @@
 package com.ll;
 
-import java.util.Arrays;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 class Saying{
@@ -17,17 +19,95 @@ class Saying{
         this.id = count++;
         System.out.println(this.id + "번 명언이 등록되었습니다.");
     }
+
+    Saying(int id, String content, String author){
+        this.content = content;
+        this.author = author;
+        this.id = id;
+    }
 }
 
 public class Main {
+    static Saying findSaying(int targetNum, ArrayList<Saying> sayings){ // 객체 찾기
+        for(Saying saying : sayings){
+            if(saying.id == targetNum){
+                return saying;
+            }
+        }
+        return null;
+    }
+
+    static void saveFile(Saying saying) { // 파일 저장
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("ID = ").append(saying.id).append(" / ")
+                .append("author = ").append(saying.author).append(" / ")
+                .append("content = ").append(saying.content);
+
+        try(FileWriter writer = new FileWriter("DB/" + saying.id + ".txt")){
+            writer.write(stringBuilder.toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    static void delFile(int targetNum) { // 파일 삭제
+        File file = new File("DB/" + targetNum + ".txt");
+        if(file.delete()){
+        }else{
+            System.out.println("파일을 삭제하지 못했습니다.");
+        }
+    }
+
+    static ArrayList<Saying> sortList(ArrayList<Saying> sayings) { // 배열 정렬하기
+        ArrayList<Saying> newList = new ArrayList<>();
+        for(int i = 0 ; i < Saying.count-1 ; i++){
+            newList.add(new Saying());
+        }
+        for(Saying saying : sayings){
+            newList.set(newList.size() - saying.id, saying);
+        }
+        while(newList.remove(findSaying(0,newList))){}
+        return sayings = newList;
+    }
+
+    static void readDB(ArrayList<Saying> sayings) { // 지난 파일 읽어오기
+        File[] files = new File("DB").listFiles();
+        if(files != null){
+            for(File file : files){
+                try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
+                    String fileContent = bufferedReader.readLine();
+                    String[] elementAList = fileContent.split("/");
+                    List<String> list = new ArrayList<>();
+
+                    for(String elementA : elementAList){
+                        String[] elementBList = elementA.split("=");
+                        list.add(elementBList[1].strip());
+                    }
+
+                    Saying savingFromBefore = new Saying(Integer.parseInt(list.get(0)), list.get(2), list.get(1));
+                    sayings.add(savingFromBefore);
+
+                    if(sayings.getLast().id + 1 > Saying.count){
+                        Saying.count = sayings.getLast().id + 1;
+                    }
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
 
     public static void main(String[] args) {
-        System.out.println("== 명언 앱 ==");
         Scanner scanner = new Scanner(System.in);
         String command;
         String content;
         String author;
-        Saying[] sayings = {};
+        ArrayList<Saying> sayings = new ArrayList<>();
+
+        readDB(sayings); // 기존 내용 불러옴
+
+        System.out.println("== 명언 앱 ==");
 
         while (true) {
             System.out.print("명령 : ");
@@ -42,56 +122,66 @@ public class Main {
                 System.out.print("작가 : ");
                 author = scanner.nextLine();
                 Saying saying = new Saying(content, author);
-                sayings = Arrays.copyOf(sayings, sayings.length +1);
-                sayings[sayings.length-1] = saying;
+                sayings.addFirst(saying);
+
+                // 폴더 확인 및 만들기
+                File folder = new File("DB");
+                if(!folder.exists()){
+                    folder.mkdir();
+                }
+
+                // 파일 생성
+                saveFile(saying);
 
             }else if (command.equals("목록")) {
                 System.out.println("번호 / 작가 / 명언");
                 System.out.println("------------------");
 
-                for(Saying s : sayings){
-                    if(s == null){
-                        continue;
-                    }else {
-                        System.out.println(s.id + " / " + s.author + " / " + s.content);
-                    }
+                for(Saying s : sortList(sayings)) {
+                    System.out.println(s.id + " / " + s.author + " / " + s.content);
                 }
-
-            }else if (command.equals("삭제")) {
-                System.out.println("명령어는 삭제? id = 의 형식으로 입력해주세요.");
 
             }else if (command.startsWith("삭제? id =")) {
                 String[] parts = command.split("=");
                 int delNo = Integer.parseInt(parts[1].strip());
 
-                if(sayings[delNo-1] != null){
-                    sayings[delNo-1] = null;
-                }else{
+                Saying targetSaying = findSaying(delNo, sayings);
+                if(targetSaying == null){
                     System.out.println(delNo + "번 명언은 존재하지 않습니다.");
-                    continue;
+                }else{
+                    sayings.remove(findSaying(delNo, sayings));
+                    System.out.println(delNo + "번 명언이 삭제되었습니다.");
+
+                    delFile(delNo); // 파일도 삭제
                 }
-
-                System.out.println(delNo + "번 명언이 삭제되었습니다.");
-
-            }else if (command.equals("수정")) {
-                System.out.println("명령어는 수정? id = 의 형식으로 입력해주세요.");
 
             }else if (command.startsWith("수정? id =")) {
                 String[] parts = command.split("=");
                 int modNo = Integer.parseInt(parts[1].strip());
 
-                if(sayings[modNo -1] != null){
-                    System.out.println("명언(기존) : " + sayings[modNo-1].content);
-                    System.out.print("명언 : ");
-                    sayings[modNo-1].content = scanner.nextLine();
-                    System.out.println("작가(기존) : " + sayings[modNo-1].author);
-                    System.out.print("작가 : ");
-                    sayings[modNo-1].author = scanner.nextLine();
-                }else{
+                Saying targetSaying = findSaying(modNo, sayings);
+                if(targetSaying == null){
                     System.out.println(modNo + "번 명언은 존재하지 않습니다.");
-                    continue;
+                }else{
+                    System.out.println("명언(기존) : " + targetSaying.content);
+                    System.out.print("명언 : ");
+                    targetSaying.content = scanner.nextLine();
+
+                    System.out.println("작가(기존) : " + targetSaying.author);
+                    System.out.print("작가 : ");
+                    targetSaying.author = scanner.nextLine();
+
+                    saveFile(targetSaying);
+
+                    System.out.println(modNo + "번 명언이 수정되었습니다.");
                 }
-                System.out.println(modNo + "번 명언이 수정되었습니다.");
+
+            //잘못된 명령 입력 모음
+            }else if (command.equals("삭제")) {
+                System.out.println("명령어는 삭제? id = 의 형식으로 입력해주세요.");
+
+            }else if (command.equals("수정")) {
+                System.out.println("명령어는 수정? id = 의 형식으로 입력해주세요.");
 
             }else{
                 System.out.println("잘못된 명령입니다.");
